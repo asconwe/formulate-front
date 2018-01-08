abd1d3bc42a8dca2780da87c22ce401234302899
August Conwell <august.conwell@gmail.com>
2018-01-08 00:14:47 -0500

    Return one element instead of entire library

--- src/components/scaffold/element-library/index.js
+++ src/components/scaffold/element-library/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import Box from './Box';
+import CreateCustomElementContainer from './CustomElementContainer';
 import DraftTextLine from './DraftTextLine';
 import DraftTextArea from './DraftTextArea';
 // import LiveText from './LiveText'
 
-export const DraftHeader = ({setValue, value}) => (<h2><DraftTextLine setValue={setValue} value={value}/></h2>)
+export const DraftHeader = ({ setValue, value }) => (<h2><DraftTextLine setValue={setValue} value={value} /></h2>)
 
 export const DraftParagraph = ({ setValue, value }) => (<p><DraftTextArea setValue={setValue} value={value} /></p>)
 
-export default {
-  Box,
-  DraftHeader,
-  DraftParagraph
+const elementLibrary = (mode, type, id) => {
+  const elements = {
+    live: {
+      Box,
+      // Header: LiveHeader,
+      // Paragraph: LiveParagraph
+    },
+    draft:
+      {
+        Box,
+        Header: CreateCustomElementContainer(DraftHeader, id),
+        Paragraph: CreateCustomElementContainer(DraftParagraph, id)
+      }
+  }
+
+  return elements[mode][type];
 }
+
+export default elementLibrary
