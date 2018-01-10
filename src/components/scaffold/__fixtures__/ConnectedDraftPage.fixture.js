ee7717855ccbd2e1422a14aafb996fb1a132ac71
August Conwell <august.conwell@gmail.com>
2018-01-09 23:05:22 -0500

    Detailed elements state

--- src/components/scaffold/__fixtures__/ConnectedDraftPage.fixture.js
+++ src/components/scaffold/__fixtures__/ConnectedDraftPage.fixture.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import ConnectedDraftPage from '../ConnectedDraftPage';
+import ConnectedDraftPage from '../container-components/ConnectedDraftPage';
 
 export default {
     component: ConnectedDraftPage,
     reduxState: {
-        structureContent: [null, null, null,
-            {
-                id: 3,
-                value: 'test'
-            },
-            {
-                id: 3,
-                value: 'test'
-            }
-        ],
-        title: 'my page',
-        structure: [
+        elements: [
             {
                 id: 0,
                 type: "Box",
@@ -25,42 +14,68 @@ export default {
                         margin: "3px"
                     }
                 },
+            },
+            {
+                id: 1,
+                type: "Box",
+                options: {
+                    style: {
+                        background: "yellow",
+                        margin: "3px"
+                    }
+                },
+            },
+            {
+                id: 2,
+                type: "Box",
+                options: {
+                    style: {
+                        background: "blue",
+                        margin: "3px"
+                    }
+                },
+            },
+            {
+                id: 3,
+                type: "Header",
+                value: 'help',
+                options: {
+                    style: {}
+                }
+            },
+            {
+                id: 4,
+                type: "Paragraph",
+                value: 'test',
+                options: {
+                    style: {}
+                }
+            }
+        ],
+
+        title: 'my page',
+        structure: [
+            {
+                id: 0,
+                type: 'Box',
                 children: [
                     {
                         id: 1,
-                        type: "Box",
-                        options: {
-                            style: {
-                                background: "yellow",
-                                margin: "3px"
-                            }
-                        },
+                        type: 'Box',
                         children: [
                             {
                                 id: 3,
-                                type: "Header",
-                                options: {
-                                    style: {}
-                                }
+                                type: 'Header'
                             }
                         ]
                     },
                     {
                         id: 2,
-                        type: "Box",
-                        options: {
-                            style: {
-                                background: "blue",
-                                margin: "3px"
-                            }
-                        },
+                        type: 'Box',
                         children: [
                             {
                                 id: 4,
-                                type: "Paragraph",
-                                options: {
-                                    style: {}
-                                }
+                                type: 'Paragraph',
                             }
                         ]
                     }
