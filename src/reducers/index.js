0c9a50f2fcf5c5f5cecdea3bceba01559b042d7e
August Conwell <august.conwell@gmail.com>
2018-01-09 23:07:33 -0500

    rename structureContent to elements

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,17 @@
 const initialState = {
-    structureContent: []
+    elements: []
 }
 
 function structure(state, action) {
     switch (action.type) {
         case "SET_VALUE_OF_ELEMENT":
-            const element = Object.assign({}, state.structureContent[action.id], { value: action.text });
-            const newStructureContent = 
-                state.structureContent.slice(0, action.id)
+            const element = Object.assign({}, state.elements[action.id], { value: action.text });
+            const newElements = 
+                state.elements.slice(0, action.id)
                 .concat(element)
-                .concat(state.structureContent.slice(action.id + 1));
+                .concat(state.elements.slice(action.id + 1));
             return Object.assign({}, state, {
-                structureContent: newStructureContent
+                elements: newElements
             })
         default:
             return state;
